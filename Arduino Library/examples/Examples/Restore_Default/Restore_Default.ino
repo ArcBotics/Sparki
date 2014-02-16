54009af9887ebbe631da098383919f23dd50bc9f
Joseph Schlesinger <joe@arcbotics.com>
2014-02-17 07:57:34 +0800

    2nd though, Removed edge avoidance from default code

--- Arduino Library/examples/Examples/Restore_Default/Restore_Default.ino
+++ Arduino Library/examples/Examples/Restore_Default/Restore_Default.ino
@@ -70,24 +70,7 @@ void loop() {
             sparki.moveRight(30);
         }
     }
-    
-  int edgeLeft   = sparki.edgeLeft();   // measure the left edge IR sensor
-  int edgeRight  = sparki.edgeRight();  // measure the right edge IR sensor
-  
-  int threshold = 200; // if below this value, no surface underneath
-  
-  if (edgeLeft < threshold) // if no surface underneath left sensor
-  {
-    sparki.moveBackward(5);
-    sparki.moveRight(20); // turn right
-  }
-
-  if (edgeRight < threshold) // if no surface underneath right sensor
-  {
-    sparki.moveBackward(5);
-    sparki.moveLeft(20); // turn left
-  }     
-    
+        
     delay(100); // wait 0.1 seconds (100 milliseconds)
   }
 }
