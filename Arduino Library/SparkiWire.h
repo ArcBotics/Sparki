0e9672ebe8a415effd74e51088a82cbe034c5b60
Joseph Schlesinger <joe@arcbotics.com>
2014-05-16 05:59:20 -0700

    Roll twi.c and twi.h into SparkiWire
    Solves weird dependency/inclusion/compilation issues

--- Arduino Library/SparkiWire.h
+++ Arduino Library/SparkiWire.h
@@ -26,6 +26,34 @@
 #include "Stream.h"
 
 #define BUFFER_LENGTH 32
+#include <inttypes.h>
+
+//#define ATMEGA8
+
+#ifndef TWI_FREQ
+#define TWI_FREQ 100000L
+#endif
+
+#ifndef TWI_BUFFER_LENGTH
+#define TWI_BUFFER_LENGTH 32
+#endif
+
+#define TWI_READY 0
+#define TWI_MRX   1
+#define TWI_MTX   2
+#define TWI_SRX   3
+#define TWI_STX   4
+
+void twi_init(void);
+void twi_setAddress(uint8_t);
+uint8_t twi_readFrom(uint8_t, uint8_t*, uint8_t, uint8_t);
+uint8_t twi_writeTo(uint8_t, uint8_t*, uint8_t, uint8_t, uint8_t);
+uint8_t twi_transmit(const uint8_t*, uint8_t);
+void twi_attachSlaveRxEvent( void (*)(uint8_t*, int) );
+void twi_attachSlaveTxEvent( void (*)(void) );
+void twi_reply(uint8_t);
+void twi_stop(void);
+void twi_releaseBus(void);
 
 class TwoWire : public Stream
 {
