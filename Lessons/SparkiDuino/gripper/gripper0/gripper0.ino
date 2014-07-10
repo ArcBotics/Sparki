59781a90765d02022a46927703976376a5cc9d96
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-07-10 16:37:04 -0500

    Added "Basic Gripper Usage" (SparkiDuino) examples.

---
+++ Lessons/SparkiDuino/gripper/gripper0/gripper0.ino
@@ -0,0 +1,12 @@
+#include <Sparki.h> // include the sparki library
+
+void setup()
+{
+  sparki.gripperOpen();  // open the robot's gripper
+  delay(1500);           // for 1.5 seconds (1500 milliseconds)
+  sparki.gripperStop();
+}
+
+void loop()
+{
+}
