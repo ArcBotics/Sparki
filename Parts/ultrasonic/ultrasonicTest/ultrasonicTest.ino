1909af0e4bcf3f567be8eca05cefcf438abbbc64
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-07-17 10:26:53 -0500

    Ultrasonic" example.

---
+++ Parts/ultrasonic/ultrasonicTest/ultrasonicTest.ino
@@ -0,0 +1,36 @@
+/*******************************************
+ Basic Ultrasonic test
+
+ Show the distance Sparki's eyes are reading
+ on the LCD. Sparki will beep when something
+ is too close. If it measures -1, that means 
+ the sensor is either too close or too far 
+ from an object
+********************************************/
+#include <Sparki.h> // include the robot library
+
+void setup()
+{
+}
+
+void loop()
+{
+    sparki.clearLCD();
+
+    int cm = sparki.ping(); // measures the distance with Sparki's eyes
+
+    sparki.print("Distance: "); 
+    sparki.print(cm); // tells the distance to the computer
+    sparki.println(" cm"); 
+
+    if(cm != -1) // make sure its not too close or too far
+    { 
+        if(cm < 10) // if the distance measured is less than 10 centimeters
+        {
+            sparki.beep(); // beep!
+        }
+    }
+
+    sparki.updateLCD();
+    delay(100); // wait 0.1 seconds (100 milliseconds)
+}
