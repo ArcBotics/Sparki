61c2a1863fd363ebca1a52d48e025e5d88d34bbb
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-07-17 10:08:11 -0500

    Added "Status LED" example.

---
+++ Parts/statusLED/statusLEDTest/statusLEDTest.ino
@@ -0,0 +1,32 @@
+/*******************************************
+ Status LED test
+
+ Sparki has a red status LED, next to the
+ power on LED. This code first blinks it
+ on then off, then fades it on then off.
+********************************************/
+#include <Sparki.h>  // include the sparki library
+
+void setup()
+{
+}
+
+void loop()
+{
+    digitalWrite(STATUS_LED, HIGH); // will turn the LED on
+    delay(500); // wait 0.5 seconds
+    digitalWrite(STATUS_LED, LOW); // will turn the LED off
+    delay(500); // wait 0.5 seconds
+
+    for(int i=0; i<255; i++) // pulse the LED on
+    {
+      analogWrite(STATUS_LED, i);
+      delay(5);
+    }
+
+    for(int i=255; i>0; i--) // pulse the LED off
+    {
+      analogWrite(STATUS_LED, i); // will turn the LED 100% on
+      delay(5);
+    }
+}
