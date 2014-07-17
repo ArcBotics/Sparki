fb3e7e0ea64be08724c51972e39f432c2cab5ff5
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-07-17 10:45:23 -0500

    Added "Basic Sparki Code" code.

---
+++ Lessons/SparkiDuino/basicSparkiCode/basicSparkiCode/basicSparkiCode.ino
@@ -0,0 +1,11 @@
+#include <Sparki.h> // include the sparki library
+
+void setup() // code inside these brackets runs first, and only once
+{
+
+}
+
+void loop() // code inside these brackets runs over and over forever
+{
+
+}
