1fe830d04c80b6bbe2117f40cb1a3d9e7b17b4c2
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-07-17 10:11:35 -0500

    Added "Wheels" example.

---
+++ Parts/wheels/wheelsTest/wheelsTest.ino
@@ -0,0 +1,32 @@
+/*******************************************
+ Basic motor test
+
+ Move Sparki forward and backwards, then 
+ rotate right and left, then stop. Repeat!
+ Can you get Sparki to do any cool motions?
+ Maybe draw something by sticking a pen down
+ the center?
+********************************************/
+#include <Sparki.h> // include the robot library
+
+void setup()
+{
+}
+
+void loop()
+{
+  sparki.moveForward();// move the robot forward
+  delay(1000); // wait a second (1000 milliseconds)
+
+  sparki.moveBackward(); // move the robot backward
+  delay(1000);
+
+  sparki.moveRight(); // rotate the robot clockwise
+  delay(1000);
+
+  sparki.moveLeft(); // rotate rhe robot counter-clockwise
+  delay(1000);
+
+  sparki.moveStop(); // stop all robot wheels
+  delay(2000); // wait two seconds (2000 milliseconds)
+}
