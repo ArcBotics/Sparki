4e5b560dbc67ad9c58edc6dd782ada795de4de9c
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-07-17 22:12:23 -0500

    Added "if" code.

---
+++ Lessons/SparkiDuino/if/if/if.ino
@@ -0,0 +1,25 @@
+#include <Sparki.h> // include the sparki library
+
+void setup()
+{
+}
+
+void loop()
+{
+    int cm = sparki.ping(); // measures the distance with Sparki's eyes
+
+    if (cm < 10) // if sparki measures a distance less than 10, show red
+    {
+      sparki.RGB(RGB_RED);
+    }
+
+    if (cm == 10) // if sparki measures equal to 10, show green
+    {
+      sparki.RGB(RGB_GREEN);
+    }
+
+    if (cm > 10) // if sparki measures a distance greater than 10, show show
+    {
+      sparki.RGB(RGB_BLUE);
+    }
+}
