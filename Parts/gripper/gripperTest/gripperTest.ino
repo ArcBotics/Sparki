e5dd473f7378daa7775165bb1dab3f0cb0823963
Joseph Schlesinger <joe@arcbotics.com>
2014-08-07 22:17:28 +0800

    Updated gripper example

--- Parts/gripper/gripperTest/gripperTest.ino
+++ Parts/gripper/gripperTest/gripperTest.ino
@@ -1,11 +1,13 @@
 /*******************************************
  Basic Gripper test
-
+ 
  Sparki has two little grippers it can be 
  used to grab objects and drag them around.
  See what you can grab with them!
+ 
+ http://arcbotics.com/products/sparki/parts/gripper/
 ********************************************/
-#include <Sparki.h> // include the robot library
+#include <Sparki.h> // include the sparki library
 
 void setup()
 {
@@ -15,10 +17,14 @@ void loop()
 {
   sparki.gripperOpen();  // open the robot's gripper
   delay(1000);           // for 1 second (1000 milliseconds)
-
+  
   sparki.gripperClose(); // close the robot's gripper
   delay(1000);           // for 1 second (1000 milliseconds)
-
+  
   sparki.gripperStop();  // stop the grippers from moving
   delay(1000);           // for 1 second (1000 milliseconds)
+
+  sparki.gripperOpen(3);  // open the robot's gripper by 3 centimeters
+  sparki.gripperClose(3); // close the robot's gripper by 3 centimeters
+  delay(1000);           // wait 1 second (1000 milliseconds)
 }
