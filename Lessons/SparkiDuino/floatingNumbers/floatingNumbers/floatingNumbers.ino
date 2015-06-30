6eab398dbbe737edb27104691869587798dbc63b
Linz Craig <linzeecraig@gmail.com>
2015-06-30 07:59:21 -0600

    Changed floating numbers sketch
    Changed code so it doesn’t allude to profanity

--- Lessons/SparkiDuino/floatingNumbers/floatingNumbers/floatingNumbers.ino
+++ Lessons/SparkiDuino/floatingNumbers/floatingNumbers/floatingNumbers.ino
@@ -12,22 +12,22 @@ void loop()
 
   foo = 10; // assigns the value of 10.0 to foo
 
-  float bar = 4; // declare a float with the value of 4.0
+  float blerg = 4; // declare a float with the value of 4.0
 
   sparki.print( "foo: "); 
   sparki.println( foo  );
 
-  sparki.print( "bar: ");
-  sparki.println( bar ); 
+  sparki.print( "blerg: ");
+  sparki.println( blerg ); 
 
-  sparki.print( "foo + bar: ");
-  sparki.println( foo + bar ); // foo added to bar
+  sparki.print( "foo + blerg: ");
+  sparki.println( foo + blerg ); // foo added to blerg
 
-  sparki.print( "foo * bar: ");
-  sparki.println( foo * bar ); // foo multiplied times bar
+  sparki.print( "foo * blerg: ");
+  sparki.println( foo * blerg ); // foo multiplied times blerg
 
-  sparki.print( "foo / bar: ");
-  sparki.println( foo / bar ); // foo divided by bar
+  sparki.print( "foo / blerg: ");
+  sparki.println( foo / blerg ); // foo divided by blerg
 
   sparki.updateLCD(); // put what has been drawn onto the screen
   delay(1000); // wait 1 seconds (1000 milliseconds)
